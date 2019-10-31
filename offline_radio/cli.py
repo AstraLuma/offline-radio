909849cdeaf19e4957868a79cd9a60f12aaa11e7
Jamie Bliss <jamie@ivyleav.es>
2019-10-31 16:08:05 -0400

    Attempt to duplicate stuff so we're not redownloading stuff all the time

--- offline_radio/cli.py
+++ offline_radio/cli.py
@@ -1,6 +1,7 @@
 import toml
 import feedparser
 import subprocess
+import sys
 
 
 def mk_feed_urls(subs):
@@ -16,20 +17,55 @@ def mk_feed_urls(subs):
             raise ValueError(f"Unknown sub: {sub!r}")
 
 
+class SeenList:
+    """
+    Manages the list of "seen" URLs
+    """
+    def __enter__(self):
+        try:
+            with open('.offline-radio.state', 'rt') as f:
+                self._urls = set(f)
+        except FileNotFoundError:
+            self._urls = set()
+
+        self._seen = set()
+
+        return self
+
+    def __exit__(self, *_):
+        with open('.offline-radio.state', 'wt') as f:
+            for u in self._seen:
+                print(u, file=f)
+
+    def has_seen(self, url):
+        return url in self._urls or url in self._seen
+
+    def add(self, url):
+        self._seen.add(url)
+
+
 def main():
     with open('.offline-radio.toml', 'rt') as cf:
         config = toml.load(cf)
 
-    for url in mk_feed_urls(config['sub']):
-        feed = feedparser.parse(url)
-        for ent in feed.entries:
-            # TODO: Check if we've looked at this file before
-            subprocess.run(
-                [
-                    'youtube-dl', '--add-metadata', '--xattrs',
-                    '--extract-audio', '--audio-quality', '0',
-                    ent.link,
-                ],
-            )
+    with SeenList() as sl:
+        for url in mk_feed_urls(config['sub']):
+            feed = feedparser.parse(url)
+            for ent in feed.entries:
+                if not sl.has_seen(ent.link):
+                    # FIXME: Don't add to list unless downloaded successfully
+                    proc = subprocess.run(
+                        [
+                            'youtube-dl', '--add-metadata', '--xattrs',
+                            '--match-filter', '!is_live',
+                            '--extract-audio', '--audio-quality', '0',
+                            '--quiet',
+                            ent.link,
+                        ],
+                    )
+                    if proc.returncode:
+                        print(f"Error downloading {ent['title']} ({ent['link']})", file=sys.stderr)
+                    else:
+                        sl.add(ent.link)
 
     # TODO: Apply quotas
