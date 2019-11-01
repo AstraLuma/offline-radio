e0e45bea4ac544bdf998e3d4ca8160d8d7aad64e
Jamie Bliss <jamie@ivyleav.es>
2019-11-01 13:06:57 -0400

    Implement limits

--- offline_radio/cli.py
+++ offline_radio/cli.py
@@ -1,7 +1,9 @@
 import toml
 import feedparser
+from pathlib import Path
 import subprocess
 import sys
+import time
 
 
 def mk_feed_urls(subs):
@@ -17,10 +19,39 @@ def mk_feed_urls(subs):
             raise ValueError(f"Unknown sub: {sub!r}")
 
 
+def pathtime(p):
+    """
+    Get the earliest known time for this file
+    """
+    s = p.stat()
+    return min((
+        s.st_atime,
+        s.st_mtime,
+        s.st_ctime,
+    ))
+
+
+def iter_media():
+    for f in Path().iterdir():
+        if f.startswith('.'):
+            continue
+        if not f.is_file():
+            continue
+        yield f
+
+
 class SeenList:
     """
     Manages the list of "seen" URLs
     """
+    # So the way this works is that we load the seen list, and save the seen
+    # list on exit.
+    # To keep the list at a managable size, we prune everything not seen this
+    # round, on the assumption that it's fallen out of the feed and won't come
+    # back.
+
+    # The way this shakes out is we keep two groups, seen last time and seen
+    # this time.
     def __enter__(self):
         try:
             with open('.offline-radio.state', 'rt') as f:
@@ -44,16 +75,65 @@ class SeenList:
         self._seen.add(url)
 
 
+def remove_old(maxage):
+    """
+    Remove files that are too old
+    """
+    mintime = time.time() + maxage
+    for f in iter_media():
+        if f.startswith('.'):
+            continue
+        if not f.is_file():
+            continue
+
+        if pathtime(f) < mintime:
+            print(f"Deleting {f}")
+            f.unlink()
+
+
+def iter_until_total(seq, max):
+    total = 0
+    for k, n in seq:
+        total += n
+        if total < max:
+            yield k, n
+        else:
+            break
+
+
+def remove_big(maxsize):
+    """
+    Remove files until we're under the max size, oldest first
+    """
+    files = sorted([
+        (p, p.stat().st_size)
+        for p in iter_media()
+    ], key=lambda i: pathtime(i[0]))
+    totalsize = sum(s for _, s in files)
+    if totalsize > maxsize:
+        keep = {
+            p
+            for p, _ in iter_until_total(files, maxsize)
+        }
+        for f, _ in files:
+            if f not in keep:
+                print(f"Deleting {f}")
+                f.unlink()
+
+
 def main():
+    # Load config
     with open('.offline-radio.toml', 'rt') as cf:
         config = toml.load(cf)
 
+    # Scan feeds and download new items
     with SeenList() as sl:
         for url in mk_feed_urls(config['sub']):
             feed = feedparser.parse(url)
             for ent in feed.entries:
                 if not sl.has_seen(ent.link):
-                    # FIXME: Don't add to list unless downloaded successfully
+                    print(f"Downloading {ent['title']} ({ent['link']})", file=sys.stderr)
+                    # XXX: How do direct and non-service links work?
                     proc = subprocess.run(
                         [
                             'youtube-dl', '--add-metadata', '--xattrs',
@@ -68,4 +148,13 @@ def main():
                     else:
                         sl.add(ent.link)
 
-    # TODO: Apply quotas
+    # Now apply limits
+    # First, delete everything too old
+    maxdays = config.get('limits', {}).get('age', None)
+    if maxdays is not None:
+        remove_old(maxdays * 86_400)
+
+    # Now scan for filesize
+    maxsize = config.get('limits', {}).get('size', None)
+    if maxsize is not None:
+        remove_big(maxsize)
