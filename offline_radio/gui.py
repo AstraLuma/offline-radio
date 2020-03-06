94a62166d538d8ee66cc2c696883c3e7bdcd1027
Jamie Bliss <jamie@ivyleav.es>
2020-03-06 00:44:04 -0500

    gui: Refactor so the python picks the next track, and automatically run the CLI in the background

--- offline_radio/gui.py
+++ offline_radio/gui.py
@@ -2,36 +2,40 @@ import base64
 import importlib.resources
 import mimetypes
 import os
+import random
 import subprocess
 import tempfile
+import threading
 
+import pause
 import webview
 import xattr
 
+from .cli import main as run_downloader
+
 
 class JsApi:
-    def read_track_data(self):
-        rv = []
-        for file in os.listdir():
-            if file.startswith('.'):
-                continue
-            if not os.path.isfile(file):
-                continue
-            x = xattr.xattr(file)
+    def get_next_track(self):
+        file = random.choice([
+            file
+            for file in os.listdir()
+            if not file.startswith('.') and os.path.isfile(file)
+        ])
 
-            def g(name):
-                try:
-                    return x.get(name).decode('utf-8')
-                except (OSError, IOError):
-                    pass
+        x = xattr.xattr(file)
 
-            rv.append({
-                'filename': file,
-                'title': g('user.dublincore.title'),
-                'channel': g('user.dublincore.contributor'),
-                'desc': g('user.dublincore.description'),
-            })
-        return rv
+        def g(name):
+            try:
+                return x.get(name).decode('utf-8')
+            except (OSError, IOError):
+                pass
+
+        return {
+            'filename': file,
+            'title': g('user.dublincore.title'),
+            'channel': g('user.dublincore.contributor'),
+            'desc': g('user.dublincore.description'),
+        }
 
     def read_track_contents(self, filename):
         """
@@ -40,22 +44,30 @@ class JsApi:
         if '/' in filename:
             raise ValueError("Other directories not allowed")
 
-        ctype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
         with tempfile.NamedTemporaryFile(suffix='.mp3') as ntf:
             subprocess.run(
                 ['ffmpeg', '-y', '-loglevel', 'warning', '-i', filename, ntf.name],
                 check=True, stdin=subprocess.DEVNULL,
             )
             data = ntf.read()
+            ctype = mimetypes.guess_type(ntf.name)[0] or 'application/octet-stream'
 
         return f"data:{ctype};base64,{base64.b64encode(data).decode('ascii')}"
 
 
+def scanner_thread():
+    while True:
+        run_downloader()
+        pause.hours(12 * random.random())
+
+
 def start():
+    threading.Thread(target=scanner_thread, daemon=True).start()
     webview.create_window(
         'Radio',
         html=importlib.resources.read_text('offline_radio', 'page.html'),
         js_api=JsApi(),
+        width=500, height=200,
     )
     webview.start(debug=True)
 
